4c46a33ff403264088ce41bdf1d6fc616cdb5808
SwagMessiah02 <vmdemesquita@gmail.com>
2025-12-01 16:06:09 -0300

    Corrigindo problema do código

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Login from "./pages/Login"
+import Register from "./pages/Register"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route path="/" element={ <> <Header /> <Home /> </> } />
       <Route path="/login" element={ <> <Login /> </> } />
+      <Route path="/register" element={<> <Register/> </>} />
     </Routes>  
   </BrowserRouter>
 }
